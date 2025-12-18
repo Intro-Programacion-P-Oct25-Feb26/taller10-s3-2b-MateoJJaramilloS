b26ec5e178c78a6bd64c0f111b2d4b43353e7842
MateoJJaramilloS <mateojaramillosalinas@gmail.com>
2025-12-18 11:53:02 -0500

    commit

--- Taller10/src/paquete3/Problema03.java
+++ Taller10/src/paquete3/Problema03.java
@@ -10,37 +10,38 @@ package paquete3;
  * @author reroes
  */
 public class Problema03 {
+
     public static void main(String[] args) {
         // TODO code application logic here
         boolean[][] sensores = {
-    {true, true, true, true, true, true},
-    {true, false, true, false, true, false},
-    {false, false, false, false, false, false},
-    {true, true, false, true, true, false}
-};
+            {true, true, true, true, true, true},
+            {true, false, true, false, true, false},
+            {false, false, false, false, false, false},
+            {true, true, false, true, true, false}
+        };
         int sin_fallos = 0;
-        String zona_fallos = "";
-        
-         for (int i = 0; i < sensores.length; i++) {
-        int contador = 0;
+        String con_fallos = "";
 
-        for (int j = 0; j < sensores[i].length; j++) {
-            if (sensores[i][j]) {
-                contador+=1;
+        for (int i = 0; i < sensores.length; i++) {
+            int contador_true = 0;
+
+            for (int j = 0; j < sensores[i].length; j++) {
+                if (sensores[i][j]) {
+                    contador_true += 1;
+                }
+            }
+
+            // Si el número de true es igual al total de revisiones → sin fallos
+            if (contador_true == sensores[i].length) {
+                sin_fallos += 1;
+            } else {
+                con_fallos += "Zona " + (i + 1) + "\n";
             }
         }
 
+        String mensaje = String.format("Zonas sin fallos: Zona %d\nZonas con al menos un fallo:\n%s",
+                sin_fallos,
+                con_fallos);
 
-        if (contador == sensores[i].length) {
-            sin_fallos+=1;
-        } else {
-            zona_fallos += "Zona " + (i + 1) + "\n";
-        }
+        System.out.println(mensaje);
     }
-
-    System.out.println("Número de zonas que funcionan sin fallos: " + sin_fallos);
-    System.out.println("Zonas con al menos un fallo:");
-    System.out.println(zona_fallos);
-}
-        
-}
